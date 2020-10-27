96b5cb11a1dbfed5c05998d92a307079d7eb446f
Simão Pedro <simao.s.pedro@gmail.com>
2020-10-27 18:27:45 +0000

    Added columns to users table necessary to sign up and login

--- paws4adoption_web/console/migrations/m130524_201442_init.php
+++ paws4adoption_web/console/migrations/m130524_201442_init.php
@@ -6,13 +6,13 @@ class m130524_201442_init extends Migration
 {
     public function up()
     {
-        $tableOptions = null;
+        /*$tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable('{{%user}}', [
+        $this->createTable('{{%users}}', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
@@ -23,7 +23,15 @@ class m130524_201442_init extends Migration
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
-        ], $tableOptions);
+        ], $tableOptions);*/
+
+        $this->addColumn('{{%users}}', 'username', $this->string()->notNull()->unique());
+        $this->addColumn('{{%users}}', 'auth_key', $this->string(32)->notNull());
+        $this->addColumn('{{%users}}', 'password_hash', $this->string()->notNull());
+        $this->addColumn('{{%users}}', 'password_reset_token', $this->string()->unique());
+        $this->addColumn('{{%users}}', 'status', $this->smallInteger()->notNull()->defaultValue(10));
+        $this->addColumn('{{%users}}', 'created_at', $this->integer()->notNull());
+        $this->addColumn('{{%users}}', 'updated_at', $this->integer()->notNull());
     }
 
     public function down()
