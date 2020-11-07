cdbdb20ed06c3835ce7d4895f50bc78e9a0d138d
Simão Pedro <simao.s.pedro@gmail.com>
2020-11-07 16:31:05 +0000

    Removed the img html

--- paws4adoption_web/frontend/views/missing-animal/missingAnimalForm.php
+++ paws4adoption_web/frontend/views/missing-animal/missingAnimalForm.php
@@ -12,36 +12,23 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
 
-    <div class="row">
-        <div class="col-sm-auto">
-            <?= $form->field($model, 'sex')->radioList(\common\models\Animal::getSex()) ?>
-            <?= $form->field($model, 'nature_id')->dropDownList(\common\models\Nature::getData()) ?>
-            <?= $form->field($model, 'fur_length_id')->dropDownList(\common\models\FurLength::getData()) ?>
-            <?= $form->field($model, 'fur_color_id')->dropDownList(\common\models\FurColor::getData()) ?>
-            <?= $form->field($model, 'size_id')->dropDownList(\common\models\Size::getData()) ?>
-            <?= $form->field($model, 'chipId') ?>
-            <?= $form->field($model, 'missing_date')->widget(\yii\jui\DatePicker::classname(), [
-                'language' => 'pt',
-                'dateFormat' => 'dd-MM-yyyy',
-            ]) ?>
-            <?= $form->field($model, 'is_missing')->checkbox() ?>
-            <?= $form->field($model, 'description')->textarea() ?>
-            <?= $form->field($model, 'img')->fileInput()?>
+        <?= $form->field($model, 'sex')->radioList(\common\models\Animal::getSex()) ?>
+        <?= $form->field($model, 'nature_id')->dropDownList(\common\models\Nature::getData()) ?>
+        <?= $form->field($model, 'fur_length_id')->dropDownList(\common\models\FurLength::getData()) ?>
+        <?= $form->field($model, 'fur_color_id')->dropDownList(\common\models\FurColor::getData()) ?>
+        <?= $form->field($model, 'size_id')->dropDownList(\common\models\Size::getData()) ?>
+        <?= $form->field($model, 'chipId') ?>
+        <?= $form->field($model, 'missing_date')->widget(\yii\jui\DatePicker::classname(), [
+            'language' => 'pt',
+            'dateFormat' => 'dd-MM-yyyy',
+        ]) ?>
+        <?= $form->field($model, 'is_missing')->checkbox() ?>
+        <?= $form->field($model, 'description')->textarea() ?>
+        <?= $form->field($model, 'img')->fileInput()?>
 
-        </div>
 
-        <div class="col-sm-auto">
-
-            <div class="img">
-                <?= Html::img('', ['alt' => 'Foto do animal'])?>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="form-group">
-            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
-        </div>
+    <div class="form-group">
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
