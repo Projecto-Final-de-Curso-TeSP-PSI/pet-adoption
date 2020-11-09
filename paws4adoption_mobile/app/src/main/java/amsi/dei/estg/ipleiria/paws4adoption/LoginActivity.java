4aa2725024de15bcab5742209488554703b71937
Ricardo <2190787@my.ipleiria.pt>
2020-11-09 21:50:32 +0000

    PROJ-142 [Mobile] [View] Login

---
+++ paws4adoption_mobile/app/src/main/java/amsi/dei/estg/ipleiria/paws4adoption/LoginActivity.java
@@ -0,0 +1,27 @@
+package amsi.dei.estg.ipleiria.paws4adoption;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmail;
+    private EditText etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+    }
+
+}
