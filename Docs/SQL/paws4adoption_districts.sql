7d63a2de4e8b1ca7af510a10fc9ec559f6351ce1
Simão Pedro <2192579@my.ipleiria.pt>
2020-11-13 19:44:18 +0000

    PROJ-161 Inserted data into the districts table and the address table #done

---
+++ Docs/SQL/paws4adoption_districts.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: paws4adoption
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `districts`
+--
+
+LOCK TABLES `districts` WRITE;
+/*!40000 ALTER TABLE `districts` DISABLE KEYS */;
+INSERT INTO `districts` VALUES (1,'Aveiro'),(2,'Beja'),(3,'Braga'),(4,'Bragança'),(5,'Castelo Branco'),(6,'Coimbra'),(7,'Évora'),(8,'Faro'),(9,'Guarda'),(10,'Leiria'),(11,'Lisboa'),(12,'Portalegre'),(13,'Porto'),(14,'Santarém'),(15,'Setúbal'),(16,'Viana do Castelo'),(17,'Vila Real'),(18,'Viseu'),(19,'Ilha da Madeira'),(20,'Ilha de Porto Santo'),(21,'Ilha de Santa Maria'),(22,'Ilha de São Miguel'),(23,'Ilha Terceira'),(24,'Ilha da Graciosa'),(25,'Ilha de São Jorge'),(26,'Ilha do Pico'),(27,'Ilha do Faial'),(28,'Ilha das Flores'),(29,'Ilha do Corvo');
+/*!40000 ALTER TABLE `districts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-13 19:29:44
