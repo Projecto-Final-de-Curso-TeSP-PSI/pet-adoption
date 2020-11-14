55ed685d4f2efaa643cc454d333f8faddc6fe2cd
ClaudiaSofia <2190781@my.ipleiria.pt>
2020-11-14 00:19:23 +0000

    add scripts

---
+++ Docs/SQL/address.sql
@@ -0,0 +1,28 @@
+/*
+-- Query: SELECT * FROM paws4adoption.address
+LIMIT 0, 1000
+
+-- Date: 2020-11-14 00:16
+*/
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (1,'Rua da Escola','7','2.º drt',2400,102,'Leiria',10);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (2,'Av. da Liberdade','10A','4.º esq',1059,765,'Lisboa',11);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (3,'Praceta D. João Pereira','56',NULL,3025,22,'Coimbra',6);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (4,'Bairro 1.º de Maio','25',NULL,2140,57,'Chamusca',14);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (5,'Estrada de Fátima','107',NULL,2395,1,'Minde',14);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (6,'Estrada do Caldeirão','8','1.º frt',9980,33,'Corvo',29);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (7,'Rua General Humberto Delgado','20','3-º esq',2555,100,'Entroncamento',14);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (8,'Avenida Santa Joana Princesa','15A',NULL,2566,526,'Lisboa',11);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (9,'Rua de Quintãs','294',NULL,2654,111,'Penafiel',13);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (10,'Rua da Carreira','56','6º drt',1500,159,'Braga',3);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (11,'Rua Castanheiros','26','2º drt',2565,154,'Mirandela',4);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (12,'Rua Marques Pombal','53','3º esq',2410,121,'Leiria',10);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (13,'Rua Alferes Veiga Pestana','10',NULL,9270,129,'Ribeira Funda',19);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (14,'Rua Cruzes','48',NULL,4750,462,'Santo Amaro',3);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (15,'R Doutor Alberto Sampaio','28','3º drt',4820,145,'Fafe',3);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (16,'Rua Nossa Senhora de Fatima','33',NULL,3400,149,'Oliveira do Hospital',6);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (17,'Rua do Casal','100',NULL,2410,68,'Leiria',10);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (18,'Rua de Santa Isabel','7','4º esq',2450,125,'Fatima',10);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (19,'Rua da Chamusca','5',NULL,5244,54,'Chamusca',14);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (20,'Avenida Luis Bívar','85','',1050,243,'Lisboa',11);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (21,'Rua de Pombal','21',NULL,2458,548,'Pombal',10);
+INSERT INTO `` (`id`,`street`,`door_number`,`floor`,`postal_code`,`street_code`,`city`,`district_id`) VALUES (22,'Rua de Coimbra','5','2º drt',3000,256,'Coimbra',6);
