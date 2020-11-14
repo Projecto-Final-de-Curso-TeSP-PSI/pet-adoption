5fb6caba0010299b1fe984fd10fa2aa0c301ec61
ClaudiaSofia <2190781@my.ipleiria.pt>
2020-11-14 00:20:25 +0000

    PROJ-165 add script organizations #done

---
+++ Docs/SQL/organizations.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM paws4adoption.organizations
+LIMIT 0, 1000
+
+-- Date: 2020-11-14 00:14
+*/
+INSERT INTO `` (`id`,`name`,`nif`,`email`,`phone`,`address_id`) VALUES (1,'Ajuda Animal','546235957','ajudaanimal@gmail.com','919 305 5',21);
+INSERT INTO `` (`id`,`name`,`nif`,`email`,`phone`,`address_id`) VALUES (2,'Gatos Urbanos','510709575','gatos.urbanos@gmail.com','915632562',22);
+INSERT INTO `` (`id`,`name`,`nif`,`email`,`phone`,`address_id`) VALUES (3,'Associação Zoófila de Leiria','532145265','azleiria.geral@gmail.com','913555456',17);
+INSERT INTO `` (`id`,`name`,`nif`,`email`,`phone`,`address_id`) VALUES (4,'Apaaf Associação','534426426','apaaf1@gmail.com','915420452',18);
+INSERT INTO `` (`id`,`name`,`nif`,`email`,`phone`,`address_id`) VALUES (5,'Associação Amigos dos Animais Chamusca','597234636','animaischamusca@hotmail.com','915344265',19);
+INSERT INTO `` (`id`,`name`,`nif`,`email`,`phone`,`address_id`) VALUES (6,'Associação Zoófila Portuguesa','537185687','info@azp.pt','217 970 8',20);
