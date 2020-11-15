d91eead8369446555693685a00fe52e40b13ddf8
Simão Pedro <2192579@my.ipleiria.pt>
2020-11-15 22:31:58 +0000

    PROJ-149 Created OrgDetailActivity #in-progress

---
+++ paws4adoption_mobile/app/src/main/java/amsi/dei/estg/ipleiria/paws4adoption/OrgDetailActivity.java
@@ -0,0 +1,14 @@
+package amsi.dei.estg.ipleiria.paws4adoption;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrgDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_org_detail);
+    }
+}
