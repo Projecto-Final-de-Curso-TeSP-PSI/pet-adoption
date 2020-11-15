afd9b96a507f7894f0afe25b3914bffcf56e04e7
Simão Pedro <2192579@my.ipleiria.pt>
2020-11-15 19:52:19 +0000

    PROJ-148 Created OrganizationActivity #in-progress

---
+++ paws4adoption_mobile/app/src/main/java/amsi/dei/estg/ipleiria/paws4adoption/OrganizationActivity.java
@@ -0,0 +1,14 @@
+package amsi.dei.estg.ipleiria.paws4adoption;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrganizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organization);
+    }
+}
