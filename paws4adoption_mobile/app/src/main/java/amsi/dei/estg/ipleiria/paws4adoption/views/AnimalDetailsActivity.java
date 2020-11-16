eb57f6c8213a7cf7731db630fd5e013543cdc589
Ricardo <2190787@my.ipleiria.pt>
2020-11-16 19:41:05 +0000

    PROJ-146 Mobile Animal Details

---
+++ paws4adoption_mobile/app/src/main/java/amsi/dei/estg/ipleiria/paws4adoption/views/AnimalDetailsActivity.java
@@ -0,0 +1,18 @@
+package amsi.dei.estg.ipleiria.paws4adoption.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import amsi.dei.estg.ipleiria.paws4adoption.R;
+
+public class AnimalDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_details);
+    }
+
+
+}
