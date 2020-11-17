84f9429f800e8ca8b23f46bbed3951a6150f1554
Ricardo <2190787@my.ipleiria.pt>
2020-11-17 12:44:15 +0000

    Component Panel

---
+++ paws4adoption_web/frontend/views/components/_panel.php
@@ -0,0 +1,21 @@
+<?php
+
+/* @var $title */
+/* @var $content */
+
+?>
+
+<div class="row">
+    <div class="col-xl-12">
+        <div class="card mb-30">
+            <div class="card-body">
+                <div class="card-header">
+                    <h5 class="card-title"><?=$title?></h5>
+                </div>
+
+                <?= $content ?>
+
+            </div>
+        </div>
+    </div>
+</div>
