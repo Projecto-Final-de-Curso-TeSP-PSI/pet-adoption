2d6e5872c4757138903f6a4e6fed39ece26feb63
Cláudia <2190781@my.ipleiria.com>
2020-11-18 01:22:42 +0000

    adjustment AdoptionAnimal Details

--- paws4adoption_web/frontend/views/adoption-animal/_itemListAdoptAnimal.php
+++ paws4adoption_web/frontend/views/adoption-animal/_itemListAdoptAnimal.php
@@ -10,22 +10,15 @@ use yii\helpers\HtmlPurifier;
 <div class="card mb-30">
     <?= Html::img('@web/images/cards/1.jpg', ['alt' => 'Card Image', 'class' => 'card-img radius-0']); ?>
     <div class="card-body">
-        <div class="card-title h5">Nome</div>
+        <div class="card-title h5"><?= HtmlPurifier::process($model->animal->name) ?></div>
         <hr class="lineCard">
         <div class="divInfo">
-            <p class="card-text">Raça: <?= HtmlPurifier::process($model->animal->nature->name) ?></p>
-            <p class="card-text">Sexo: <?= HtmlPurifier::process($model->animal->sex) ?></p>
-            <p class="card-text">Porte: <?= HtmlPurifier::process($model->animal->size->size) ?></p>
-            <p class="card-text">Colocado em: <?= HtmlPurifier::process($model->animal->createdAt) ?></p>
-
-            <!-- <p class="card-text">Especie:</p><p><?= HtmlPurifier::process($model->animal->nature->nameByParentId) ?></p>
-                <p class="card-text">Pelo:</p><p><?= HtmlPurifier::process($model->animal->furLength->fur_length) ?></p>
-                <p class="card-text">Cor:</p><p><?= HtmlPurifier::process($model->animal->furColor->fur_color) ?></p>
-                <p class="card-text">Organização:</p><p><?= HtmlPurifier::process($model->organization->name) ?></p>
-                <p class="card-text">Descrição:</p><p><?= HtmlPurifier::process($model->animal->description) ?></p> -->
-
-
+            <p class="card-text"><b>Raça: </b><?= HtmlPurifier::process($model->animal->nature->name) ?></p>
+            <p class="card-text"><b>Sexo: </b><?= HtmlPurifier::process($model->animal->sex) ?></p>
+            <p class="card-text"><b>Porte: </b> <?= HtmlPurifier::process($model->animal->size->size) ?></p>
+            <p class="card-text"><b>Colocado em: </b><?= HtmlPurifier::process($model->animal->createdAt) ?></p>
         </div>
+
         <hr class="lineCard">
         <?= Html::button('Mais informação', [
             'class' => 'btn btn-primary btn-success',
@@ -33,23 +26,26 @@ use yii\helpers\HtmlPurifier;
             'data-toggle' => 'modal',
             'data-target' => '#modalAnimalDetails',
         ]) ?>
+
         <!-- Modal dos Detalhes -->
         <?= Yii::$app->view->renderFile('@frontend/views/components/_modalAnimalDetails.php',
-            ['title' => 'Detalhes',
+            ['title' => 'Detalhes do Animal',
+                'type'=> $model->getType(),
+                'name'=> $model->animal->name,
+                'nature' => $model->animal->nature->nameByParentId,
+                'bread' => $model->animal->nature->name,
+                'sex' => $model->animal->sex,
+                'size' => $model->animal->size->size,
+                'color' => $model->animal->furColor->fur_color,
+                'furLength' => $model->animal->furLength->fur_length,
+                'chip' => $model->animal->chipId,
+                'date' => $model->animal->createdAt,
+                'postBy' => $model->organization->name,
+                'description' => $model->animal->description,
 
-                'nature' =>$model->animal->nature->nameByParentId,
-                'bread' =>$model->animal->nature->name,
-                'sex' =>$model->animal->sex,
-                'size' =>$model->animal->size->size,
-                'color' =>$model->animal->furColor->fur_color,
-                'furLength' =>$model->animal->furLength->fur_length,
-                'chip' =>$model->animal->chipId,
-                'date' =>$model->animal->createdAt,
-                'postBy' =>$model->organization->name,
-                'description' =>$model->animal->description,
-
-                'btn1' => 'Adotar',
-                'btn2' => 'FAT'
+                'submitAdoption' => 'Adotar',
+                'submitFat' => 'FAT',
+                'closeText' => 'Fechar'
             ]); ?>
     </div>
 </div>
