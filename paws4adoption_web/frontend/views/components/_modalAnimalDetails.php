4d81221ebb1d023cac70dc893c90dd06203eb539
Cláudia <2190781@my.ipleiria.com>
2020-11-18 01:18:37 +0000

    add _modalDetailsAnimal

--- paws4adoption_web/frontend/views/components/_modalAnimalDetails.php
+++ paws4adoption_web/frontend/views/components/_modalAnimalDetails.php
@@ -1,8 +1,11 @@
 <?php
+
 use yii\helpers\Html;
 
 
+/* @var $type */
 /* @var $title */
+/* @var $name */
 /* @var $nature */
 /* @var $bread */
 /* @var $sex */
@@ -13,9 +16,20 @@ use yii\helpers\Html;
 /* @var $date */
 /* @var $postBy */
 /* @var $description */
+/* @var $missingDate */
+/* @var $foundDate */
+/* @var $location */
+
+/* @var $submitAdoption */
+/* @var $submitFat */
+/* @var $submitContact */
+/* @var $closeText */
+
+
 ?>
 
-<div class="modal fade LargeModal" id="modalAnimalDetails" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+<div class="modal fade LargeModal" id="modalAnimalDetails" tabindex="-1" role="dialog"
+     aria-labelledby="exampleModalLabel"
      aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
@@ -29,31 +43,43 @@ use yii\helpers\Html;
 
                 <?= Html::img('@web/images/cards/1.jpg', ['alt' => 'Card Image', 'class' => 'card-img radius-0']); ?>
                 <div class="card-body">
-                    <div class="card-title h5">Nome</div>
+                    <div class="card-title h5"><?= $name ?> </div>
                     <hr class="lineCard">
                     <div class="divInfo divInfoModel">
                         <div class="divInfoModelSec">
-                        <p class="card-text"><b>Especie: </b><?= $nature ?></p>
-                        <hr>
-                        <p class="card-text"><b>Raça: </b><?= $bread ?></p>
-                        <hr>
-                        <p class="card-text"><b>Sexo: </b><?= $sex ?></p>
-                        <hr>
-                        <p class="card-text"><b>Chip: </b><?= $chip ?></p>
+                            <p class="card-text"><b>Especie: </b><?= $nature ?></p>
+                            <hr>
+                            <p class="card-text"><b>Raça: </b><?= $bread ?></p>
+                            <hr>
+                            <p class="card-text"><b>Sexo: </b><?= $sex ?></p>
+                            <hr>
+                            <p class="card-text"><b>Chip: </b><?= $chip ?></p>
                         </div>
-
                         <div class="divInfoModelSec">
-                        <p class="card-text"><b>Cor: </b><?= $color ?></p>
-                        <hr>
-                        <p class="card-text"><b>Pelagem: </b><?= $furLength ?></p>
-                        <hr>
-                        <p class="card-text"><b>Porte: </b><?= $size ?></p>
-                        <hr>
-                        <p class="card-text"><b>Postado por: </b><?= $postBy ?></p>
+                            <p class="card-text"><b>Cor: </b><?= $color ?></p>
+                            <hr>
+                            <p class="card-text"><b>Pelagem: </b><?= $furLength ?></p>
+                            <hr>
+                            <p class="card-text"><b>Porte: </b><?= $size ?></p>
+                            <hr>
+                            <p class="card-text"><b>Postado por: </b><?= $postBy ?></p>
                         </div>
                     </div>
                     <hr>
-                    <p class="card-text pDate"><b><?= $date ?></b></p>
+                    <?php
+                    switch ($type) {
+                        case 'missingAnimal':
+                            echo '<p class="card-text pDate"><b>Desaparceu em: </b>' . $missingDate . '</p>';
+                            break;
+                        case 'foundAnimal':
+                            echo '<p class="card-text pDate"><b>Encontrado em: </b>' . $foundDate . '</p>';
+                            echo '<p class="card-text pDate"><b>Local: </b>' . $location . '</p>';
+                            break;
+                        default:
+                            break;
+                    }
+                    ?>
+                    <p class="card-text pDate"><b>Post feito: </b><?= $date ?></p>
                     <hr>
                     <p class="card-text"><b>Descricao: </b></p>
                     <p><?= $description ?></p>
@@ -61,8 +87,17 @@ use yii\helpers\Html;
 
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal"><?= $btn1 ?></button>
-                <button id="btnModalSubmit" type="button" class="btn btn-primary"><?= $btn2 ?></button>
+                <?php
+                if ($type == 'adoptionAnimal') {
+                    echo '<button id="btnModalSubmit" type="button" class="btn btn-primary"> ' . $submitAdoption . '</button>';
+                    echo '<button id="btnModalSubmit" type="button" class="btn btn-primary"> ' . $submitFat . '</button>';
+                    echo '<button type="button" class="btn btn-secondary" data-dismiss="modal"> ' . $closeText . '</button>';
+
+                } else {
+                    echo '<button id="btnModalSubmit" type="button" class="btn btn-primary"> ' . $submitContact . '</button>';
+                    echo '<button type="button" class="btn btn-secondary" data-dismiss="modal"> ' . $closeText . '</button>';
+                }
+                ?>
             </div>
         </div>
     </div>
