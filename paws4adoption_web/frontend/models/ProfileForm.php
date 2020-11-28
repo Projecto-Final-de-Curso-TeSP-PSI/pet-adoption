8a022c1b02e20ee0ce00a666b0715936ea06b1c3
Simão Pedro <2192579@my.ipleiria.pt>
2020-11-28 15:34:41 +0000

    PROJ-25 Bug fix #in-progress

--- paws4adoption_web/frontend/models/ProfileForm.php
+++ paws4adoption_web/frontend/models/ProfileForm.php
@@ -24,22 +24,23 @@ class ProfileForm extends Model
     public $city;
     public $district_id;
 
-    private User $user;
-    private Address $address;
+//    private $loggedUser;
+//    private $loggedUserAddress;
 
     public function rules()
     {
-        $this->user = new User();
-        $this->address = new Address();
-        $userRules = $this->user->rules();
-        $addressRules = $this->address->rules();
+//        $loggedUser_id = \Yii::$app->user->id;
+//        $this->loggedUser = User::findOne($loggedUser_id);
+//        $this->loggedUserAddress = isset($this->loggedUser->address_id) ? Address::findOne($this->loggedUser->address_id) : new Address();
+        $userRules = $this->loggedUser->rules();
+        $addressRules = $this->loggedUserAddress->rules();
         return array_merge($userRules, $addressRules);
     }
 
     public function attributeLabels()
     {
-        $userAttributeLabels = $this->user->attributeLabels();
-        $addressAttributeLabels = $this->address->attributeLabels();
+        $userAttributeLabels = $this->loggedUser->attributeLabels();
+        $addressAttributeLabels = $this->loggedUserAddress->attributeLabels();
         return array_merge($userAttributeLabels, $addressAttributeLabels);
     }
 
@@ -50,24 +51,24 @@ class ProfileForm extends Model
 //        }
 
         $loggedUser_id = \Yii::$app->user->id;
-        $loggedUser = User::findOne($loggedUser_id);
+//        $this->loggedUser = User::findOne($loggedUser_id);
 
-        $this->address->street = $this->street;
-        $this->address->door_number = $this->door_number;
-        $this->address->floor = $this->floor;
-        $this->address->postal_code = $this->postal_code;
-        $this->address->street_code = $this->street_code;
-        $this->address->city = $this->city;
-        $this->address->district_id = $this->district_id;
-        $this->address->save(false);
-
-        $loggedUser->firstName = $this->firstName;
-        $loggedUser->lastName = $this->lastName;
-        $loggedUser->nif = $this->nif;
-        $loggedUser->phone = $this->phone;
-        $loggedUser->email = $this->email;
-        $loggedUser->address_id = $this->address->id;
-
-        return $loggedUser->save(false);
+//        $this->loggedUserAddress->street = $this->street;
+//        $this->loggedUserAddress->door_number = $this->door_number;
+//        $this->loggedUserAddress->floor = $this->floor;
+//        $this->loggedUserAddress->postal_code = $this->postal_code;
+//        $this->loggedUserAddress->street_code = $this->street_code;
+//        $this->loggedUserAddress->city = $this->city;
+//        $this->loggedUserAddress->district_id = $this->district_id;
+//        $this->loggedUserAddress->save();
+//
+//        $this->loggedUser->firstName = $this->firstName;
+//        $this->loggedUser->lastName = $this->lastName;
+//        $this->loggedUser->nif = $this->nif;
+//        $this->loggedUser->phone = $this->phone;
+//        $this->loggedUser->email = $this->email;
+//        $this->loggedUser->address_id = $this->loggedUserAddress->id;
+//
+//        return $this->loggedUser->save(false);
     }
 }
