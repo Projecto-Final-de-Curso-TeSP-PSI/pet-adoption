df9c8a56ee50fab8ea5cff020df4e618f83b4b87
Ricardo <2190787@my.ipleiria.pt>
2020-11-29 18:53:24 +0000

    PROJ-178 acceptance tests signin signup
    not finished

--- paws4adoption_web/frontend/tests/acceptance/HomeCest.php
+++ paws4adoption_web/frontend/tests/acceptance/HomeCest.php
@@ -1,6 +1,7 @@
 <?php
 namespace frontend\tests\acceptance;
 
+use Codeception\Util\Locator;
 use frontend\tests\AcceptanceTester;
 use yii\helpers\Url;
 
@@ -8,13 +9,23 @@ class HomeCest
 {
     public function checkHome(AcceptanceTester $I)
     {
-        $I->amOnPage(Url::toRoute('/site/index'));
-        $I->see('My Application');
 
-        $I->seeLink('About');
-        $I->click('About');
+
+    }
+
+    public function CheckSidebarLinks(AcceptanceTester $I){
+
+        $I->amOnPage(Url::toRoute('/site/index'));
         $I->wait(2); // wait for page to be opened
 
-        $I->see('This is the About page.');
+        $I->see('No que consiste o site');
+
+        $I->seeInSource('<span class="middle-bar"></span>');
+        $I->click(Locator::find('span', ['class' => 'middle-bar']));
+        //$I->click(['css' => '.middle-bar']);
+
+        $I->click(Locator::find('a', ['href' => '/organization']));
+        $I->see('Associações');
+
     }
 }
---
+++ paws4adoption_web/frontend/tests/acceptance/LoginCest.php
@@ -0,0 +1,38 @@
+<?php namespace frontend\tests\acceptance;
+use Codeception\Util\Locator;
+use frontend\tests\AcceptanceTester;
+use yii\helpers\Url;
+
+class LoginCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function Login(AcceptanceTester $I)
+    {
+        $I->wantTo("test making a login");
+
+        $I->amOnPage(Url::toRoute('/site/index'));
+        $I->wait(2); // wait for page to be opened
+
+        $I->see('No que consiste o site');
+
+        $I->click(Locator::find('span', ['class' => 'name']));
+
+        $I->see('LogIn');
+
+        $I->click(Locator::find('a', ['href' => '/site/login']));
+        $I->seeInCurrentUrl('/site/login');
+        $I->see('Log In');
+
+
+        $I->fillField('LoginForm[username]', 'ricardolopes');
+        $I->fillField('LoginForm[password]', 'Sporting');
+        $I->click('login-button');
+
+        $I->wait(1);
+        $I->see('Ricardo Lopes');
+    }
+}
